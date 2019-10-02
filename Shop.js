d287d93698c046aca8bf39b0d6c2faf4d63b1491
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-10-01 23:19:49 -0400

    added cart modal with paypal buttons.

--- Shop.js
+++ Shop.js
@@ -2,76 +2,72 @@ let products = {
     glasses: [{
         id: "glass1",
         price: 100,
-        image: "",
+        image: "glass1",
         gender: "M"
     },
     {
         id: "glass3",
         price: 100,
-        image: "",
+        image: "glass2",
         gender: "M"
     },
     {
         id: "glass5",
         price: 100,
-        image: "",
+        image: "glass3",
         gender: "M"
     },
     {
         id: "glass4",
         price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
-        gender: "M"
-    },
-    {
-        id: "glass1",
-        price: 100,
-        image: "",
+        image: "glass3",
         gender: "M"
     },
     {
         id: "glass2",
-        price: 200,
+        price: 100,
+        image: "glass2",
+        gender: "M"
+    },
+    {
+        id: "glass6",
+        price: 100,
+        image: "glass1",
+        gender: "M"
+    },
+    {
+        id: "glass7",
+        price: 100,
         image: "",
-        gender: "F"
+        gender: "M"
+    },
+    {
+        id: "glass8",
+        price: 100,
+        image: "",
+        gender: "M"
     }],
     accessories: []
+}
+
+let cart = []
+
+const addToCart = function(e) {
+    e.preventDefault();
+    console.log(this.id);
+    let updateName = this.id;
+
+    for ( let i = 0; i < products.glasses.length; i++) {
+        if(updateName === products.glasses[i].id) {
+
+            let purchase = {
+                name: products.glasses[i].id,
+                cost: products.glasses[i].price
+            };
+            cart.push(purchase);
+        }
+    }
+    console.log(cart);
 }
 
 const renderProducts = function() {
@@ -83,9 +79,7 @@ const renderProducts = function() {
         if (products.glasses[i].gender === "M") {
     $(`#mensProdTable`).append(`
         <div class="mensGlass row container" id="glasses${i}">
-            <div class="col-4" id="glassImg">
-                <p>I will show an image</p>
-            </div>
+            <div class="col-4" id="glassImg" style="background-image: url('./Assets/Glasses/${products.glasses[i].image}.jpg');"></div>
             <div class="col-8" id="prodInfo">
                 <span>${products.glasses[i].id}</span> <span>${products.glasses[i].price}</span>
                 <p>;kjjklkjd;akfj;ka;kfj;kfja;kfj;kfak;lf
@@ -93,11 +87,13 @@ const renderProducts = function() {
                     kldfk;ajfk;lfj;ajf;fjkaf;lfjadkf;fj;af
                     ;lkajfk;jfkfj;j;afjjfjdjf;afj;klfja;lkf
                 </p>
+                <button class="btn btn-success" id="${products.glasses[i].id}">Add to Cart</button>
         </div>
     `)
     } else {
         return;
     } 
+    $(`#${products.glasses[i].id}`).on("click", addToCart);
 }
 }
 
