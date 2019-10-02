158c28017981ede71103540444849e036aa216b0
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-10-01 20:34:03 -0400

    basic render function working for mens ONLY

--- Shop.js
+++ Shop.js
@@ -1,4 +1,75 @@
-// showItem = function () {
-//     console.log("I'm working");
+// let products = {
+//         mens: [{
+//             glasses: [{
+//                 id: "glass1",
+//                 price: 10,
+//                 image: "N/A",
+//             },
+//             {
+//                 id: "glass2",
+//                 price: 10,
+//                 image: "N/A"
+//             }],
+//             accesories: [{}],
+//         }],
+//         womens: [{
+//             glasses: [{
+//                 id: "glass1",
+//                 price: 15,
+//                 image: "N/A",
+//             },
+//             {
+//                 id: "glass2",
+//                 price: 15,
+//                 image: "N/A"
+//             }],
+//             accesories: [{}],
+//         }],       
+        
 // }
-// document.getElementById("mens1").addEventListener("click", showItem);
+
+let mensProducts = {
+        glasses: [{
+            id: "glass1",
+            price: 10,
+            image: "N/A"
+        },
+        {
+            id: "glass2",
+            price: 10,
+            image: "N/A"
+        }],
+        accessories: [],
+}
+
+let womensProducts = {
+    glasses: [{
+        id: "glass1",
+        price: 15,
+        image: "N/A"
+    },
+    {
+        id: "glass2",
+        price: 15,
+        image: "N/A"
+    }],
+    accessories: [],
+}
+
+const renderProducts = function() {
+    console.log(mensProducts);
+    console.log(this);
+
+    $(`#mensProdTable`).empty();
+    for ( let i = 0; i < mensProducts.glasses.length; i++) {
+    $(`#mensProdTable`).append(`
+        <div class="col-lg-2 mensGlass" id="glasses${i}">
+            <button type="button" class="btn btn-info btn-lg" id="${mensProducts.glasses[i].id}">Open Modal</button>
+            <p>Price is: ${mensProducts.glasses[i].price}</p>
+        </div>
+    `)
+    }
+
+}
+
+renderProducts();
